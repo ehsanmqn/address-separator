38a3949762a7e2c538d4ccf103f8bceb686d5313
Ehsan <spin313@gmail.com>
2023-04-21 20:32:48 +0330

    The AddressSeparator class added

---
+++ address_separator/address_separator.py
@@ -0,0 +1,19 @@
+from .domain.address import Address
+from .infra.validator import Validator
+
+
+class AddressSeparator:
+    """
+    The AddressSeparator class is responsible for separating the street name and house number from the address string.
+    """
+    def __init__(self):
+        self.validator = Validator()
+
+    def separate(self, address_str):
+        # validate address_str before processing
+        if not self.validator.validate(address_str):
+            return {}
+
+        # parse address into street and house number using Address class
+        address = Address.from_string(address_str)
+        return {"street": address.street, "housenumber": address.house_number}
