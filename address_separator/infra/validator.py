69687171a5eba17d765f000e7a01b5a31eb81ef2
Ehsan <spin313@gmail.com>
2023-04-23 12:13:50 +0330

    Docucomment updated

--- address_separator/infra/validator.py
+++ address_separator/infra/validator.py
@@ -1,7 +1,7 @@
 
 class Validator:
     """
-    The validator class for validating input addree
+    The validator class for validating input address
     """
     def validate(self, address_str):
         # To be implemented in future
