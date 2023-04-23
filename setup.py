78ed68d3a5f24589b29f43adbb030c7f7ca5729b
Ehsan <spin313@gmail.com>
2023-04-23 12:42:16 +0330

    Files moved to upper level dir

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='address-separator',
+setup(name='address_separator',
       version='1.0',
       description='Address parser',
       author='Ehsan Maiqani',
